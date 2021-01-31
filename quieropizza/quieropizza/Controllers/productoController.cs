821bc18d6d4a6d3d88c2760d4f24ca14b9ea0079
Ritaramos40 <Ritaramos40@RAMOS>
2021-01-31 17:42:44 -0600

    agregamos una lista de productos

---
+++ quieropizza/quieropizza/Controllers/productoController.cs
@@ -0,0 +1,32 @@
+﻿using quieropizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quieropizza.Controllers
+{
+    public class productoController : Controller
+    {
+        // GET: producto
+        public ActionResult Index()
+        {
+            var producto1 = new productomodel();
+            producto1.Id = 1;
+            producto1.Descripcion = "pizza 6 quesos";          
+            var producto2 = new productomodel();
+            producto2.Id = 2;
+            producto2.Descripcion = "pizza 4 estaciones ";
+            var producto3 = new productomodel();
+            producto3.Id = 3;
+            producto3.Descripcion = "pizza jamon y queso";
+            var listadeProductos = new List<productomodel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }
+    }
+}
